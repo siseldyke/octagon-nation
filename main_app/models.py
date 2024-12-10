b6faa6448ecb55766fb324cb2d74f2fc7d4b6d1a
siseldyke <siseldyke@gmail.com>
2024-12-10 10:13:25 -0500

    successful migration of event model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)  
+    weight_class = models.DecimalField(max_digits=5, decimal_places=1)  
+    is_title_fight = models.BooleanField(default=False)  
+    attendees = models.PositiveIntegerField()  
+    location = models.CharField(max_length=255)  
+    date = models.DateField()  
+
+    def __str__(self):
+        return f"{self.name} - {self.location} ({self.date})"
