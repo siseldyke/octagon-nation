e0e32a4efdb915bce821356857a15bd6175d0bfe
siseldyke <siseldyke@gmail.com>
2024-12-10 13:00:12 -0500

    home page rendering

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
 # Create your views here.
 
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1>Octagon Nation</h1>')
+    return render(request, 'home.html')
