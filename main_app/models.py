a252eecd1511bdd238dda326817a71af396be6ee
siseldyke <siseldyke@gmail.com>
2024-12-10 10:39:07 -0500

    updated event model to make it simpler

--- main_app/models.py
+++ main_app/models.py
@@ -3,9 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Event(models.Model):
-    name = models.CharField(max_length=255)  
-    weight_class = models.DecimalField(max_digits=5, decimal_places=1)  
-    is_title_fight = models.BooleanField(default=False)  
+    name = models.CharField(max_length=255)   
     attendees = models.PositiveIntegerField()  
     location = models.CharField(max_length=255)  
     date = models.DateField()  
