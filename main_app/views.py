431f57c6564430340dbf7b3068acb1aa97e67884
siseldyke <siseldyke@gmail.com>
2024-12-10 09:32:57 -0500

    home route working

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Octagon Nation</h1>')
