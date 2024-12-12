8ff27feb96d1c88810d088741bc070af11079ed9
siseldyke <siseldyke@gmail.com>
2024-12-12 12:07:54 -0500

    successful create on front end

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('events/', views.event_index, name='events'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('events/create/', views.EventCreate.as_view(), name='event-create'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 from django.contrib.auth.views import LoginView
 from django.shortcuts import get_object_or_404
 from .models import Event
@@ -14,3 +15,12 @@ def event_index(request):
 def event_detail(request, event_id):
     event = Event.objects.get(id=event_id)
     return render(request, 'events/detail.html', {'event': event})
+
+class EventCreate(CreateView):
+    model = Event
+    fields = ['name', 'attendees', 'location', 'date']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    success_url = '/events/'
